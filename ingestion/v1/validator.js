2f5a2749f165fa14681313914e405a9a596ac237
JP Hastings-Spital <jphastings@blinkbox.com>
2014-06-27 10:51:55 +0100

    Regional rights

--- ingestion/v1/validator.js
+++
@@ -1,9 +0,0 @@
-var tv4 = require('tv4');
-var fs = require('fs');
-
-var schema = fs.readFileSync(__dirname + '/contributor.schema.json', "utf8");
-var data = fs.readFileSync(__dirname + '/image_upload.contributor.json', "utf8");
-
-var result = tv4.validateMultiple(data, schema);
-
-console.log(result);
